eb26965ff30bafd4001ecc57777451da078b50ef
zhlouise <louisezhou2004@gmail.com>
2026-03-24 09:50:00 +0800

    updated fresnel_zone_plot and the colormap of sat_img_plot

--- Analysis Tools/fresnel_zone_plot.m
+++ Analysis Tools/fresnel_zone_plot.m
@@ -13,70 +13,63 @@ function fresnel_zone_plot(centroid_lat,centroid_lon, a, b, azi_angle, data)
 % -------------------------------------------------------------------------
 
 % Flatten inputs to vectors
-latv = centroid_lat(:);
-lonv = centroid_lon(:);
-av = a(:);
-bv = b(:);
-azv = azi_angle(:);
-dv = data(:);
-nEllipses = numel(latv);
+centroid_lat_v = centroid_lat(:);
+centroid_lon_v = centroid_lon(:);
+a_v = a(:);
+b_v = b(:);
+az_v = azi_angle(:);
+data_v = data(:);
 
-% Filter valid data
-validIdx = ~isnan(latv) & ~isnan(lonv) & ~isnan(av) & ~isnan(bv) & ~isnan(azv) & ~isnan(dv) & (av>0) & (bv>0);
+% Filter out invalid data
+valid_idx = ~isnan(centroid_lat_v) & ~isnan(centroid_lon_v) & ~isnan(a_v) & ~isnan(b_v) & ~isnan(az_v) & ~isnan(data_v) & (a_v>0) & (b_v>0);
+centroid_lat_v = centroid_lat_v(valid_idx);
+centroid_lon_v = centroid_lon_v(valid_idx);
+a_v = a_v(valid_idx);
+b_v = b_v(valid_idx);
+az_v = az_v(valid_idx);
+data_v = data_v(valid_idx);
+num_ellipses = numel(centroid_lat_v);
 
 % Plot centroids using sat_img_plot
-pos_llh = [latv, lonv, zeros(nEllipses,1)];
-sat_img_plot(pos_llh(validIdx,:), dv(validIdx));
+pos_llh = [centroid_lat_v, centroid_lon_v, zeros(num_ellipses,1)];
+sat_img_plot(pos_llh, data_v);
 hold on;
 
 % Setup for ellipse plotting
-nPoints = 48;
-t = linspace(0, 2*pi, nPoints);
-cmap = jet(256);
-dataMin = min(dv(validIdx));
-dataMax = max(dv(validIdx));
-if dataMin == dataMax
-    dataMax = dataMin + 1;
+num_points = 30;
+t = linspace(0, 2*pi, num_points);
+cmap = turbo(256);
+data_min = min(data_v);
+data_max = max(data_v);
+% For cases when all the data is just one value to prevent errors
+if data_min == data_max
+    data_max = data_min + 1;
 end
 
 % Map data values to RGB colors
-mapValueToRGB = @(val) interp1(linspace(dataMin,dataMax,size(cmap,1))', cmap, val, 'linear', 'extrap');
+mapValueToRGB = @(val) interp1(linspace(data_min,data_max,size(cmap,1))', cmap, val, 'linear', 'extrap');
 
 % Plot each ellipse
-for k = 1:nEllipses
-    if ~validIdx(k), continue; end
+for id = 1:num_ellipses
     
-    a_k = av(k);
-    b_k = bv(k);
-    az_k = azv(k);
-    lat0 = latv(k);
-    lon0 = lonv(k);
-    
-    % Rotation angle: convert azimuth (CW from North) to theta (CCW from East)
-    theta_k = 90 - az_k;
-    
-    % Create ellipse in ENU coordinates (meters)
-    x = a_k * cos(t);  % east
-    y = b_k * sin(t);  % north
-    
+    % Convert azimuth (clockwise from North) to rotation angle (counterclockwise from East)
+    theta = 90 - az_v(id);
+    % Create ellipse in ENU coordinates (parametric expression of an ellipse)
+    x = a_v(id)*cos(t); % East
+    y = b_v(id)*sin(t); % North
     % Rotate ellipse
-    R = [cosd(theta_k) -sind(theta_k); sind(theta_k) cosd(theta_k)];
-    pts = R * [x; y];
-    east = pts(1,:);
-    north = pts(2,:);
-    
-    % Convert meters to degrees (approximate)
-    dlat = north / 111320;
-    dlon = east / (111320 * cosd(lat0));
-    latPts = lat0 + dlat;
-    lonPts = lon0 + dlon;
-    
+    R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)]; % 2D rotation matrix
+    ellipse_enu = (R*[x;y]).';
+    % Convert from ENU to llh
+    [ellipse_llh, ~] = rtklib.enu2llh([ellipse_enu, zeros(height(ellipse_enu),1)], [centroid_lat_v(id),centroid_lon_v(id),0]);
     % Get color for this ellipse
-    rgb = mapValueToRGB(dv(k));
-    
+    rgb = mapValueToRGB(data_v(id));
     % Plot ellipse boundary
-    geoplot(latPts, lonPts, 'Color', rgb, 'LineWidth', 1.5);
+    geoplot(ellipse_llh(:,1), ellipse_llh(:,2), 'Color', rgb, 'LineWidth', 1);
+
 end
+
+hold off;
 
 end
 
